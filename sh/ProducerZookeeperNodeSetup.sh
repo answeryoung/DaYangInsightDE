b82dc94a51a1c11cad8099f48c13e2f6e62a8d26
Da Yang (杨答) <answeryoung@gmail.com>
2020-06-13 18:39:13 -0400

    added references

--- sh/ProducerZookeeperNodeSetup.sh
+++ sh/ProducerZookeeperNodeSetup.sh
@@ -17,6 +17,32 @@ pip3 install boto3
 # get kafka_2.4.1
 yes | wget https://downloads.apache.org/kafka/2.4.1/kafka_2.11-2.4.1.tgz 
 tar -xzf kafka_2.11-2.4.1.tgz
-export PATH="$PATH:/home/ec2-user/kafka_2.11-2.4.1/bin"
+# export PATH="$PATH:/home/ec2-user/kafka_2.11-2.4.1/bin"
 
-python3 
+sudo mv /home/ec2-user/kafka_2.11-2.4.1/    /usr/local/kafka
+sed -i 's#dataDir=.*#dataDir=/home/ec2-user/kafka#' \
+  /usr/local/kafka/config/zookeeper.properties
+# defult clientPort=2181 
+mkdir /home/ec2-user/zookeeper
+
+# add kafka to PATH
+sudo sed -i 's#PATH=.*#PATH=$PATH:/usr/local/kafka/bin:$HOME/.local/bin:$HOME/bin#' \
+  /home/ec2-user/.bash_profile
+source /home/ec2-user/.bash_profile
+
+# setting up auto-start zookeeper
+sudo echo '/usr/local/kafka/bin/zookeeper-server-start.sh -daemon \
+  /usr/local/kafka/config/zookeeper.properties' \
+  >> /etc/rc.d/rc.local
+# sudo echo '/usr/local/kafka/bin/zookeeper-server-start.sh \
+#   /usr/local/kafka/config/zookeeper.properties \
+#    > /dev/null 2>&1 &' >> /etc/rc.d/rc.local
+sudo chmod 700 /etc/rc.d/rc.locall
+sudo systemctl enable rc-local
+sudo systemctl start rc-local  
+  
+# write some output to concole
+cat /usr/local/kafka/config/zookeeper.properties
+java --version
+scalar --version
+python3 --version
