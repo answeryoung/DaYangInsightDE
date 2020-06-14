4334ad0ee3decd41ee1ec8d7612bf03b46e11007
Da Yang (杨答) <answeryoung@gmail.com>
2020-06-14 15:58:11 -0400

    Update setup.nodes.SparkWorker.sh

--- sh/setup.nodes.SparkWorker.sh
+++ sh/setup.nodes.SparkWorker.sh
@@ -44,8 +44,8 @@ export SPARK_WORKER_CORES=8" \
 # ${sparkWorkerIps[1]} \n\
 # ${sparkWorkerIps[2]} \n\
 # ${sparkWorkerIps[3]} " \
-#   $sparkHome\conf/slaves.template \
-#   $sparkHome\conf/slaves
+#   $sparkHome/conf/slaves.template \
+#   $sparkHome/conf/slaves
 #          
 # echo ""
 # echo ""       
