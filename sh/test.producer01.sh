07f7d46a1bb120570016e778cf2f36cbe60e3f55
Da Yang (杨答) <answeryoung@gmail.com>
2020-06-21 15:58:19 -0400

    Update test.producer01.sh

--- sh/test.producer01.sh
+++ sh/test.producer01.sh
@@ -4,4 +4,4 @@ topic=test01
 kafka-topics.sh --create --zookeeper localhost:2181 \
 --topic $topic --partitions 1 --replication-factor 2
 python3 ~/src/test_producer.py $bucketName 'test1.csv' \
-  "${kafkaIps[0]}:$kafkaPort" $topic
+  "${kafkaIps[1]}:$kafkaPort" $topic
