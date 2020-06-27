487bcd4bfade74ae61cd0f461642aad573276843
Da Yang (杨答) <answeryoung@gmail.com>
2020-06-27 09:05:02 -0400

    updated test_consumer06.py

--- src/test_consumer06.py
+++ src/test_consumer06.py
@@ -12,7 +12,7 @@ db_port    = os.environ['psqlPort']
 db_su      = os.environ['db_super_usr']
 db_su_pwd  = os.environ['db_su_pwd']
 db_db_name = os.environ['dbName']
-conn = psycopg2.connect( user = db_su, password = db_su_pwd,
+conn = psycopg2.connect( user = db_su, password = db_su_pwd
                        , host = db_ip, port = db_port
                        , database = db_name)
 
@@ -22,6 +22,8 @@ from kafka import KafkaConsumer
 from json import loads
 # from pymongo import MongoClient
 import numpy as np
+import math
+from scipy.signal import find_peaks
 
 consumer = KafkaConsumer( bootstrap_servers=bootstrapServer
                         , auto_offset_reset='earliest'
@@ -45,29 +47,36 @@ log_file.writelines('Log time = ' + current_time)
 
 nMsg    = 0
 topics  = {}
+conn    = create_measurements_table(conn, table_suffix)
 for msg in consumer:
     message = msg.value
+    message['topic'] += table_suffix
     topic   = message['topic']
 
     if topic not in topics:
-        conn = create_table(conn, message)
+        conn    = create_table(conn, message)
+        topics[topic]   = None
 
-    signal  = message['signal']
-    signal  = np.array(signal.strip("[]").split(' '))
-    if len(signal) == 7:
+    if message['segment_meta']['index'] == 0:
+        topics[topic]   = message
+        topic       = create_table(conn, message)
         continue
-    else:
-        message['signal'] = signal
-
-    message = process_signal(message)
-    conn    = insert_data(conn,message)
+    
+    message_prev    = process_signal(topics[topic], message)
+    conn            = insert_data(conn, table_suffix, message_prev)
+    
+    
+    if message['segment_meta']['idx_neg'] == -1:
+        conn        = insert_data(conn, table_suffix, message)
+    
+    topics[topic] = message
     log_string = message['topic'] + ' : part: ' + str(msg.partition)
             + ', offset: ' + str(msg.offset)
             + ', idx: ' + str(i)
             + ', len: ' + str(a.size)
     print(log_string)
     log_file.writelines(log_string)
-
+    
     nMsg += 1
     if nMsg % 2000 == 0:
         print('================================================================================')
@@ -78,29 +87,29 @@ log_file.writelines('###########################################################
 log_file.close()
 
 ################################################################################
-def create_measurement_table(conn, message)
-    sql_create_table = """
+def create_measurements_table(conn, table_suffix):
+    table_name = 'measurements' + table_suffix
+    sql_create_measurements_table = """
         CREATE TABLE IF NOT EXISTS %s (
-            segment_index int
-          , P_s NUMERIC (12,4)
-          , Q_s NUMERIC (12,4)
-          , R_s NUMERIC (12,4)
-          , S_s NUMERIC (12,4)
-          , T_s NUMERIC (12,4)
-          , patient_id
-          , segment_start_time_s NUMERIC (12,4)
-          , signal signal NUMERIC (16) []
+          , patient_id varchar(32)
+          , measurement_datetime varchar(32)
+          , PR_s NUMERIC (6,4)
+          , PR_s_dist NUMERIC (6,4) []
+          , QRS_s NUMERIC (6,4)
+          , QRS_dist NUMERIC (6,4) []
+          , QT_s NUMERIC (6,4)
+          , QT_dist NUMERIC (6,4) []
+          , signal_length_s NUMERIC (12,4)
+          , signal_whole NUMERIC (16) []
           , metadata json
-          );""" % topic
+        );""" % table_name
     cur = conn.cursor()
-    cur.execute("""
-    cur.execute(sql, (idx
-      , segment_start_time_s, signal, message))
-     """)
+    cur.execute(sql_create_measurements_table)
+    conn.commit()
+    return conn
 
 def create_table(conn, message):
     topic = message['topic']
-
     sql_create_table = """
         CREATE TABLE IF NOT EXISTS %s (
             segment_index int
@@ -109,76 +118,128 @@ def create_table(conn, message):
           , R_s NUMERIC (12,4)
           , S_s NUMERIC (12,4)
           , T_s NUMERIC (12,4)
-          , patient_id
+          , PR_s NUMERIC (6,4)
+          , QRS_s NUMERIC (6,4)
+          , QT_s NUMERIC (6,4)
+          , patient_id varchar(32)
+          , measurement_datetime varchar(32)
           , segment_start_time_s NUMERIC (12,4)
-          , signal signal NUMERIC (16) []
+          , signal NUMERIC (16) []
           , metadata json
-          );""" % topic
+        );""" % topic
     cur = conn.cursor()
-    cur.execute("""
-     ? (
-      ,
-      ,
-      ,
-      , topic text
-      ,  text, reviewerName text, helpful text
-      , metadata json NOT NULL
-      ,
-
-
-    reviewerText text, overall text, summary text, unixReviewerTime text, reviewTime text)
-    ; """
-    , topic,
-    )
+    cur.execute(sql_create_table)
     conn.commit()
     return conn
 
-conn    = insert_data(conn,message,message_next)
-
-
-def insert_data(conn,message,message_next):
+def insert_data(conn, table_suffix, message):
+    measurements_table_name = 'measurements' + table_suffix
     topic       = message['topic']
     idx         = message['segment_meta']['index']
-    time_pos    = message['time_pos']
+    
     patient_id  = message['record_Meta']['name']
-    start_time_s= message['segment_start_time_s']
-    signal      = message['signal']
+    measurement_datetime = message['signal_meta']['window']
+    segment_start_time_s = message['segment_start_time_s']
+    segment_signal       = message['signal']
 
     message.pop('topic')
     message['segment_meta'].pop('index')
-    message.pop('time_pos')
     message['record_Meta'].pop('name')
+    message['signal_meta'].pop('window')
     message.pop('segment_start_time_s')
     message.pop('signal')
-
-    sql_alter_prev = """
-        INSERT INTO %s (segment_index, P_s, Q_s, R_s, S_s, T_s
-          , segment_start_time_s, signal, metadata) VALUES (
-            %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s)
-        ;""" % topic
-
+    
     cur = conn.cursor()
-    if message_next is not None:
+    if message['segment_meta']['idx_neg'] != -1:
+        time_pos = message['time_pos']
+        time_intvl = message['time_pos']
+        message.pop('time_pos')
+        message.pop('time_intvl')
         sql_insert = """
-            INSERT INTO %s (segment_index, P_s, Q_s, R_s, S_s, T_s
-              , segment_start_time_s, signal, metadata) VALUES (
-                %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s, %%s)
-            ;""" % topic
+            INSERT INTO %s (
+                segment_index
+              , P_s, Q_s, R_s, S_s, T_s, PR_s, QRS_s, QT_s
+              , patient_id, measurement_datetime
+              , segment_start_time_s, signal, metadata
+            ) VALUES (
+                %%s
+              , %%s, %%s, %%s
+              , %%s, %%s
+              , %%s, %%s, %%s
+              , %%s, %%s
+              , %%s, %%s, %%s
+            );""" % topic
         cur.execute(sql_insert, (idx
-          , time_pos['P'], time_pos['Q'], time_pos['R'],
+          , time_pos['P'], time_pos['Q'], time_pos['R']
           , time_pos['S'], time_pos['T']
-          , segment_start_time_s, signal, message))
+          , time_intvl['PR'], time_intvl['QRS'], time_intvl['QT']
+          , patient_id, measurement_datetime
+          , segment_start_time_s, segment_signal, message))
     else:
         sql_insert = """
-            INSERT INTO %s (segment_index,
-              , segment_start_time_s, signal, metadata) VALUES (
-                %%s, %%s, %%s, %%s)
-            ;""" % topic
-        cur.execute(sql, (idx
-          , segment_start_time_s, signal, message))
-
+            INSERT INTO %s (
+                segment_index,
+              , patient_id, measurement_datetime
+              , segment_start_time_s, signal, metadata
+            ) VALUES (
+                %%s
+              , %%s, %%s
+              , %%s, %%s, %%s
+            );""" % topic
+        cur.execute(sql_insert, (idx
+          , patient_id, measurement_datetime
+          , segment_start_time_s, segment_signal, message))
+        
+    if idx == 0:
+        sql_insert_measurements = """
+            INSERT INTO measurements %s (
+                patient_id, measurement_datetime, metadata
+            ) VALUES (
+                %%s, %%s, %%s
+            );""" % measurements_table_name
+        cur.execute(sql_insert, (
+            patient_id, measurement_datetime, message))
+            
     conn.commit()
     return conn
 
 
-def process_signal(message):
+def process_signal(message,message_next):
+    idx     = message['segment_meta']['index']
+    idx_neg = message_next['segment_meta']['index_neg']
+    sig_a = message['signal']
+    sig_a = np.array(sig_a.strip("[]").split(' '))
+    sig_b = message_next['signal']
+    sig_b = np.array(sig_b.strip("[]").split(' '))
+    
+    R_time_s    = message_next['segment_meta']['segment_start_time_s']
+    if len(sig_a) == 7 and len(sig_b) == 7:
+        time_pos    = { 'P': np.nan, 'Q': np.nan, 'R': np.nan
+                      , 'S': np.nan, 'T': np.nan}
+        time_intvl  = { 'PR': np.nan, 'QRS': np.nan, 'QT': np.nan}
+        
+    elif len(sig_a) == 7 or idx == 0:
+        signal = sig_b.astype(np.float)
+        time_pos    = { 'P': np.nan, 'Q': np.nan, 'R': 0
+                      , 'S': 0, 'T': 0 }
+        time_intvl  = { 'PR': np.nan, 'QRS': np.nan, 'QT': 0}
+        
+    
+    elif len(sig_b) == 7 or idx_neg == -1:
+        signal = sig_a.astype(np.float)
+        time_pos    = { 'P': 0, 'Q': 0, 'R': 0
+                      , 'S': np.nan, 'T': np.nan}
+        time_intvl  = { 'PR': 0, 'QRS': np.nan, 'QT': np.nan}
+    
+    else:
+        signal = np.concatenate((sig_a, sig_b), axis=0)
+        signal = signal.astype(np.float)
+        time_pos = {'P': 0, 'Q': 0, 'R': 0
+            , 'S': 0, 'T': 0}
+        time_intvl = {'PR': 0, 'QRS': 0, 'QT': 0}
+
+        peaks, _ = find_peaks(signal)
+        
+    message['time_pos'] = time_pos
+    message['time_pos'] = time_intvl
+    return message
